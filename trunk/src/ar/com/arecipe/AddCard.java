0dda8f2a62d4914f07e1223aaaa84151941c8ddb
caeycae <caeycae@8ed2ae6d-8124-33ed-f37f-c7a0d182afef>
2013-01-19 04:53:45 +0000

    Agrego el activity de addCard i18n para español

---
+++ trunk/src/ar/com/arecipe/AddCard.java
@@ -0,0 +1,17 @@
+package ar.com.arecipe;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class AddCard extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_card);
+		// Show the Up button in the action bar.
+	}
+}
--- trunk/src/ar/com/arecipe/MainActivity.java
+++ trunk/src/ar/com/arecipe/MainActivity.java
@@ -2,7 +2,9 @@ package ar.com.arecipe;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -14,9 +16,18 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+	    switch(item.getItemId()){
+	    case R.id.add_card:
+	        Intent intent = new Intent(MainActivity.this, AddCard.class);
+	        startActivity(intent);
+	        return true;            
+	    }
+	    return false;
+	}
 }
